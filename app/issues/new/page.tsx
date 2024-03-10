71b448bf47601bc000188bb030bcdc7f18af9c47
Pms-crypto <poorvashukla315@gmail.com>
2024-03-10 06:39:40 +0530

    error handling

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,17 +1,46 @@
 'use client';
 
-import { TextField, Button } from '@radix-ui/themes'
+import { TextField, Button, Callout } from '@radix-ui/themes'
 import SimpleMDE from "react-simplemde-editor";
 import "easymde/dist/easymde.min.css";
+import axios from 'axios';
+import { useForm, Controller } from 'react-hook-form';
+import { useRouter } from 'next/navigation';
+import { useState } from 'react';
+
+interface IssueForm {
+    title: string;
+    description: string;
+}
 
 const NewIssuePage = () => {
+    const router = useRouter()
+    const {register, control, handleSubmit} = useForm<IssueForm>();
+    const [error, setError] = useState('');
+
   return (
-    <div className="max-w-xl space-y-3">
+    <div className="max-w-xl">
+    {error && 
+        <Callout.Root color="red" className="mb-5">
+        <Callout.Text>
+          {error}
+        </Callout.Text>
+      </Callout.Root>}
+    <form className="space-y-3" onSubmit={handleSubmit(async (data)=>{
+        try {
+            await axios.post('/api/issues',data)
+            router.push('/issues')
+        } catch (error) {
+            setError("An unexpected error occured");
+        }
+    })}>
       <TextField.Root>
-        <TextField.Input placeholder="Title"/>
+        <TextField.Input placeholder="Title" {...register('title')}/>
       </TextField.Root>
-      <SimpleMDE placeholder="Type your issue" />
+      <Controller name="description" control={control} render={({field})=> <SimpleMDE placeholder="Type your issue" {...field}/>
+}/>
       <Button>Submit New Issue</Button>
+    </form>
     </div>
   )
 }
