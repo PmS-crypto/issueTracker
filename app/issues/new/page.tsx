442e2967b3b0a3eed15ba7b77d1b9f254244d0b2
Pms-crypto <poorvashukla315@gmail.com>
2024-03-10 06:01:18 +0530

    build new issue ui

---
+++ app/issues/new/page.tsx
@@ -0,0 +1,17 @@
+'use client';
+
+import { TextField, TextArea, Button } from '@radix-ui/themes'
+
+const NewIssuePage = () => {
+  return (
+    <div className="max-w-xl space-y-3">
+      <TextField.Root>
+        <TextField.Input placeholder="Title"/>
+      </TextField.Root>
+      <TextArea placeholder="Type your issue" />
+      <Button>Submit New Issue</Button>
+    </div>
+  )
+}
+
+export default NewIssuePage
